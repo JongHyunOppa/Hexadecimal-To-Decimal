b0986ebd6d50214f08d4d8195cdacf6392b82234
Amina Faradzhieva <34938706+JongHyunOppa@users.noreply.github.com>
2018-11-26 16:55:47 +0600

    Add files via upload

---
+++ hex_to_dec.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Week12_2
+{
+    //your variables and methods here...
+    Scanner input = new Scanner(System.in);
+		System.out.println("Enter hex number: ");
+		String a = input.next();
+			HexToDec(a);
+    
+    public static void main(String[] args)
+    {
+        //your code here..
+        Scanner input = new Scanner(System.in);
+		System.out.println("Enter hex number: ");
+		String a = input.next();
+			HexToDec(a);
+    }
+}
